6d57331185a26c96e0e66de42f727b8477480dd1
Viktor <viktor4strlsov@gmail.com>
2024-04-21 11:07:36 +0600

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import TheFuture from '@/components/theFuture/TheFuture'
+import TheYears from '@/components/theYears/TheYears'
+import styles from './styles/Home/Home.module.scss'
 
 
 const Home = () => {
 	return (
-		<h1>Welcome to Nexts</h1>
+		<>
+			<section className={styles.wrapperFuture}>
+				<TheFuture />
+			</section>
+			<section className={styles.wrapperYears}>
+				<TheYears />
+			</section>
+			<section></section>
+			<section></section>
+			<section></section>
+		</>
 	)
 }
 
