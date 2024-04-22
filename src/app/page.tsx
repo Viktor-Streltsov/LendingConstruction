e0cda13546cf37a32d7088c17669c8ce914ae5d3
Viktor <viktor4strlsov@gmail.com>
2024-04-22 17:14:45 +0600

    main доработка

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import TheYears from '@/components/theYears/TheYears'
 import TheFeatured from '@/components/theFeatured/TheFeatured'
 import TheRequest from '@/components/theRequest/TheRequest'
 import styles from './styles/Home/Home.module.scss'
+import CircleIcon from '@/components/icons/CircleIcon'
 
 const Home = () => {
 	return (
@@ -12,6 +13,11 @@ const Home = () => {
 				<TheFuture />
 			</section>
 			<section className={styles.wrapperYears}>
+				<ul className={styles.listInfo}>
+					<li className={styles.textList}><CircleIcon />Quality Control System, 100% Satisfaction Guarantee</li>
+					<li className={styles.textList}><CircleIcon />Highly Proffesional Staff, Accurate Testing Processes</li>
+					<li className={styles.textList}><CircleIcon />Unrivalled Workmanship, Proffesional and Qualified</li>
+				</ul>
 				<TheYears />
 			</section>
 			<section className={styles.wrapperFeatured}>
